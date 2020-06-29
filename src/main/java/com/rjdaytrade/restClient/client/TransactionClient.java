0652f0f90cf17c834889454b4becd4226cda6250
rishabhtaneja0717 <40224400+rishabhtaneja0717@users.noreply.github.com>
2020-06-28 17:12:15 -0700

    added new changes

--- src/main/java/com/rjdaytrade/restClient/client/TransactionClient.java
+++ src/main/java/com/rjdaytrade/restClient/client/TransactionClient.java
@@ -1,10 +1,14 @@
 package com.rjdaytrade.restClient.client;
 
+import java.util.Date;
 import java.util.List;
 
 import com.rjdaytrade.model.Transaction;
+import com.rjdaytrade.model.dashboard.GainSummary;
 
 public interface TransactionClient {
 	
 	List<Transaction> getTodaysTransactions(Long id);
+	
+    GainSummary getRealMonthTransactionsAfterDate(Long id, Integer month, Date date);
 }
--- src/main/java/com/rjdaytrade/restClient/client/TransactionClientImpl.java
+++ src/main/java/com/rjdaytrade/restClient/client/TransactionClientImpl.java
@@ -1,12 +1,15 @@
 package com.rjdaytrade.restClient.client;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.rjdaytrade.model.Transaction;
+import com.rjdaytrade.model.dashboard.GainSummary;
 
 @Service
 public class TransactionClientImpl extends AbstractRestClient implements TransactionClient {
@@ -14,8 +17,18 @@ public class TransactionClientImpl extends AbstractRestClient implements Transac
 	@Override
 	public List<Transaction> getTodaysTransactions(Long id) {
 		
-		ResponseEntity<Transaction[]> response = restTemplate.getForEntity(BASE_URI + "transactions/today/" + id, Transaction[].class);
+		ResponseEntity<Transaction[]> response = restTemplate.getForEntity(BASE_URI + "transactions/real/today/" + id, Transaction[].class);
 		return Arrays.asList(response.getBody());
+	}
+	
+	@Override
+	public GainSummary getRealMonthTransactionsAfterDate(Long id, Integer month, Date date) {
+		String strDate = new SimpleDateFormat("MM-dd-yyyy").format(date);
+		
+		ResponseEntity<GainSummary> response = restTemplate.getForEntity(BASE_URI + "transactions/real/month/after/" + id +"/" + month + "/" + strDate, 
+				GainSummary.class);
+		
+		return response.getBody();
 	}
 	
 }
